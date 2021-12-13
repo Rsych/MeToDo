3d928b378843199519c52a0a93544eae16a7fb1b
Ryan <tamielryan@gmail.com>
2021-12-13 20:33:04 +0900

    Initial Commit

---
+++ MeToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeToDo
+//
+//  Created by Ryan J. W. Kim on 2021/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeToDo/MeToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeToDoApp.swift
+//  MeToDo
+//
+//  Created by Ryan J. W. Kim on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct MeToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
