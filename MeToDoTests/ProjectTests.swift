7d48b40dd87da8d74cc71313e83efcb55e1d145d
Ryan <tamielryan@gmail.com>
2021-12-24 10:49:46 +0200

    ProjectTest

---
+++ MeToDoTests/ProjectTests.swift
@@ -0,0 +1,41 @@
+//
+//  ProjectTests.swift
+//  MeToDoTests
+//
+//  Created by Ryan J. W. Kim on 2021/12/24.
+//
+
+import XCTest
+import CoreData
+@testable import MeToDo
+
+class ProjectTests: BaseTestCase {
+
+    func testCreatingProjectsAndItems() {
+        let targetCount = 10
+
+        for _ in 0..<targetCount {
+            let project = Project(context: managedObjectContext)
+
+            for _ in 0..<targetCount {
+                let item = Item(context: managedObjectContext)
+                item.project = project
+            }
+        }
+
+        XCTAssertEqual(dataController.count(for: Project.fetchRequest()), targetCount)
+        XCTAssertEqual(dataController.count(for: Item.fetchRequest()), targetCount * targetCount)
+    }
+
+    func testDeletesCascade() throws {
+        try dataController.createSampleData()
+
+        let request = NSFetchRequest<Project>(entityName: "Project")
+        let projects = try managedObjectContext.fetch(request)
+
+        dataController.delete(projects[0])
+
+        XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 4)
+        XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 20)
+    }
+}
