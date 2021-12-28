bc6d9ae7a719e695a7f2753ebf9f3f0142ed39c7
Ryan <tamielryan@gmail.com>
2021-12-28 11:01:23 +0200

    ShortCut AppDelegate

--- MeToDo/Configuration/AppDelegate.swift
+++ MeToDo/Configuration/AppDelegate.swift
@@ -9,5 +9,14 @@ import Foundation
 import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-
+    // appShortcut delegate
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        return sceneConfiguration
+    }
 }
--- MeToDo/Controller/DataController.swift
+++ MeToDo/Controller/DataController.swift
@@ -257,13 +257,14 @@ class DataController: ObservableObject {
         }
     }
     /// User review
+    /// When inAppPurchase is enabled use below commented lines with new activationState.
     func appLaunched() {
         guard count(for: Project.fetchRequest()) >= 5 else { return }
 
-        let allScenes = UIApplication.shared.connectedScenes
-        // for later inApp unlock
-        //        let scene = allScenes.first { $0.activationState == .foregroundActive }
-        let scene = allScenes.first
-        if let windowScene = scene as? UIWindowScene { SKStoreReviewController.requestReview(in: windowScene)}
+//        let allScenes = UIApplication.shared.connectedScenes
+//        // for later inApp unlock
+//        //        let scene = allScenes.first { $0.activationState == .foregroundActive }
+//        let scene = allScenes.first
+//        if let windowScene = scene as? UIWindowScene { SKStoreReviewController.requestReview(in: windowScene)}
     }
 }
