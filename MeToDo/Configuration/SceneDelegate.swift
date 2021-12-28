109ecfdb004f3ff0d21062b47dcf3a7fe4730ef6
Ryan <tamielryan@gmail.com>
2021-12-28 11:02:00 +0200

    ShortCut SceneDelegate

--- MeToDo/Configuration/SceneDelegate.swift
+++ MeToDo/Configuration/SceneDelegate.swift
@@ -9,5 +9,17 @@ import Foundation
 import SwiftUI
 
 class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    @Environment(\.openURL) var openURL
 
+    func windowScene(
+        _ windowScene: UIWindowScene,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping (Bool) -> Void) {
+        guard let url = URL(string: shortcutItem.type) else {
+            completionHandler(false)
+            return
+        }
+
+        openURL(url, completion: completionHandler)
+    }
 }
