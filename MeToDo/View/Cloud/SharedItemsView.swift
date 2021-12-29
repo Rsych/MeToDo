2c9e1613d373806fc1fcc9c58495bc3b8c18e7de
Ryan <tamielryan@gmail.com>
2021-12-29 16:46:39 +0200

    Lint

--- MeToDo/View/Cloud/SharedItemsView.swift
+++ MeToDo/View/Cloud/SharedItemsView.swift
@@ -44,35 +44,35 @@ struct SharedItemsView: View {
     func fetchSharedItems() {
         guard itemsLoadState == .inactive else { return }
         itemsLoadState = .loading
-        
+
         let recordID = CKRecord.ID(recordName: project.id)
         let reference = CKRecord.Reference(recordID: recordID, action: .none)
         let pred = NSPredicate(format: "project == %@", reference)
         let sort = NSSortDescriptor(key: "title", ascending: true)
         let query = CKQuery(recordType: "Item", predicate: pred)
         query.sortDescriptors = [sort]
-        
+
         let operation = CKQueryOperation(query: query)
         operation.desiredKeys = ["title", "detail", "completed"]
         operation.resultsLimit = 50
-        
+
         operation.recordFetchedBlock = { record in
             let id = record.recordID.recordName
             let title = record["title"] as? String ?? "No title"
             let detail = record["detail"] as? String ?? ""
             let completed = record["completed"] as? Bool ?? false
-            
+
             let sharedItem = SharedItem(id: id, title: title, detail: detail, completed: completed)
             items.append(sharedItem)
             itemsLoadState = .success
         }
-        
+
         operation.queryCompletionBlock = { _, _ in
             if items.isEmpty {
                 itemsLoadState = .noResult
             }
         }
-        
+
         CKContainer.default().publicCloudDatabase.add(operation)
     }
 }
