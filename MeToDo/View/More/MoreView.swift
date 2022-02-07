18f9a850178c035654779448b493ea474de9354a
Ryan <tamielryan@gmail.com>
2022-02-07 17:33:05 +0200

    MoreViewBottomPadding

--- MeToDo/View/More/MoreView.swift
+++ MeToDo/View/More/MoreView.swift
@@ -63,6 +63,7 @@ struct MoreView: View {
                 .listRowSeparator(.hidden)
             }  //: List
             .listStyle(.plain)
+            .padding(.bottom, 44)
             .navigationTitle("More")
             
         }  //: NavView
